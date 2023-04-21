c0a7f78a6d6ca0761c5995f785162e5330978816
Sunila <sunila@techrbm.com>
2023-04-21 17:55:38 +0530

    Added all application related files - Initial commit

---
+++ BusinessLogic/GlobalExceptionhandler.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeDetails.BusinessLogic
+{
+    public class GlobalExceptionhandler
+    {
+        public static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            File.AppendAllText("error.log", e.ExceptionObject.ToString());
+
+            MessageBox.Show("An erroe has occured.Please try again later");
+        }
+    }
+}
